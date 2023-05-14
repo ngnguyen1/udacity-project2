dc7ff3f150ec034c1b3f6c8c96eff714ce21ee91
Nga Nguyen <nga.nguyen@silabs.com>
2023-05-14 15:59:18 +0700

    fix could not find MIME for Buffer issue

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import axios from "axios";
 import Jimp = require("jimp");
 
 // filterImageFromURL
@@ -11,14 +12,19 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const { data: image_buffer }= await axios({
+        method: 'get',
+        url: inputURL,
+        responseType: 'arraybuffer'
+      });
+      const photo = await Jimp.read(image_buffer);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
+        .write(__dirname + outpath, (img: any) => {
           resolve(__dirname + outpath);
         });
     } catch (error) {
